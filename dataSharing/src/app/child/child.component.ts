4eabf0994794424636ff1c2f8082fea40d847aff
tirthajagiya <tirthajagiya5618@gmail.com>
2025-01-04 15:25:21 +0530

    new

---
+++ dataSharing/src/app/child/child.component.ts
@@ -0,0 +1,33 @@
+import { NgIf } from '@angular/common';
+import { Component, Input, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-child',
+  imports: [FormsModule, NgIf],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  // parentName = input();
+  @Input() parentName: String = ''
+
+  nameChange: boolean = false
+  nameOfBtn = "Change Name"
+
+  name = output<String>();
+
+  sendData() {
+    this.name.emit(this.parentName)
+  }
+  btnClick(): void {
+    if (this.nameOfBtn == "Ok") {
+      this.nameOfBtn = "Change Name"
+      this.nameChange = false
+    }
+    else if (this.nameOfBtn == "Change Name") {
+      this.nameOfBtn = "Ok"
+      this.nameChange = true
+    }
+  }
+}
